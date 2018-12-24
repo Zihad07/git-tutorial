555d46d28f6d3aaac812dc09432fb9f4d8381847
zihad07 <zihadnayem8@gmail.com>
2018-12-24 21:03:55 +0600

    Add a method called 'disPlaySum' which display two number sum

--- addTwoNumber.js
+++ addTwoNumber.js
@@ -6,3 +6,10 @@ var value2 = 9;
 
 // print two sum
 console.log('hello world');
+
+// Call displaySum funtion
+displaySum( value1, value2);
+// 
+function displaySum(){
+    console.log("The sum is of two number is ", value1 + value2);
+}
