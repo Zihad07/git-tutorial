e6bef681401cf8e6f22d0e8c7a593b6b239c8c52
zihad07 <zihadnayem8@gmail.com>
2018-12-24 21:11:16 +0600

    add a new method, called 'displayMul' which is multiplication two number

--- addTwoNumber.js
+++ addTwoNumber.js
@@ -9,7 +9,14 @@ console.log('hello world');
 
 // Call displaySum funtion
 displaySum( value1, value2);
+
+// Call displayMul funtion
+displayMul( value1, value2);
 // 
-function displaySum(){
+function displaySum( value1, value2){
     console.log("The sum is of two number is ", value1 + value2);
+}
+
+function displayMul(value1,value2) {
+    console.log("Thw multiplication is ", value1*value2);
 }
