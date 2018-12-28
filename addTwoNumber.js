141e3cef124be982aa884622dd3de9b185dc37d5
zihad07 <zihadnayem8@gmail.com>
2018-12-28 12:46:22 +0600

    add qube funtion

--- addTwoNumber.js
+++ addTwoNumber.js
@@ -10,3 +10,37 @@ function display(){
 
 
 
+
+//  add two number
+
+var value1 = 90;
+var value2 = 9;
+
+// print two sum
+console.log('hello world');
+
+// Call displaySum funtion
+displaySum( value1, value2);
+
+// Call displayMul funtion
+displayMul( value1, value2);
+
+// Call qube funtion
+qube(value1);
+// 
+function displaySum( value1, value2){
+    console.log("The sum is of two number is ", value1 + value2);
+}
+
+function displayMul(value1,value2) {
+    console.log("Thw multiplication is ", value1*value2);
+}
+
+// Calculate square value 1
+function square(value1){
+    console.log("The squre is",value1*value1);
+}
+
+function qube(x){
+    console.log(x*x*x);
+}
