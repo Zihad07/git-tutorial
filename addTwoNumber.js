fdbb76d03ebf924fe00688aa92958ce18a44078e
zihad07 <zihadnayem8@gmail.com>
2018-12-28 12:43:31 +0600

    privious code add

--- addTwoNumber.js
+++ addTwoNumber.js
@@ -2,10 +2,22 @@
 //  add two number
 
 
-display();
+//  add two number
 
-function display(){
-    console.log('Hello US');
+var value1 = 90;
+var value2 = 9;
+
+// print two sum
+console.log('hello world');
+
+// Call displaySum funtion
+displaySum( value1, value2);
+
+// Call displayMul funtion
+displayMul( value1, value2);
+// 
+function displaySum( value1, value2){
+    console.log("The sum is of two number is ", value1 + value2);
 }
 
 function displayMul(value1,value2) {
@@ -16,3 +28,11 @@ function displayMul(value1,value2) {
 function square(value1){
     console.log("The squre is",value1*value1);
 }
+
+display();
+
+function display(){
+    console.log('Hello US');
+}
+
+
